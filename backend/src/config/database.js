ecc12825d6a2ec4230adb78c57d45077f8083da1
vaccaelpro <santiagovacca833@gmail.com>
2026-06-13 23:43:58 -0500

    Agregar puerto de DB para poder usar render y railway

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,6 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
+  port: process.env.DB_PORT || 3306,
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'sena',
   waitForConnections: true,
